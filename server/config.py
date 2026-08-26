e836a79a2215cf609a21c373c6b46cf4c7af9e4a
graceaminga-oss <grace.aminga@student.moringaschool.com>
2026-08-26 16:04:08 +0300

    fix: prepare database config for production

--- server/config.py
+++ server/config.py
@@ -7,5 +7,16 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
